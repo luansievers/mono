9600e1ccb137f2c5ac3c19e7b0ed09a11057e231
Sacha <sacha@weavik.com>
2022-09-01 18:18:08 -0400

    Makefile Updates

---
+++ setup/scripts/setup/setup_subgraph.sh
@@ -0,0 +1,23 @@
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+
+# Handle node version
+node_version=`node -v`
+export NVM_DIR=$HOME/.nvm;
+source $NVM_DIR/nvm.sh;
+
+if [ "$node_version" != "v16.15.1" ]; then
+  echo $node_version
+  nvm install 16.15.1
+fi
+
+nvm use
+npm --prefix packages/subgraph run start-local
+npm --prefix packages/subgraph run create-local
+npm --prefix packages/subgraph run deploy-local
+
+echo "${GREEN}Graph Uploaded${NC}"
+echo ""
+echo "${GREEN}Starting Client 3${NC}"
+npm --prefix packages/client3 run dev
