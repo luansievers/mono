23652dfdef60183f0ece69715afa44865bdcef72
Luan Felipe Sievers <luan.sievers@gmail.com>
2022-09-13 12:03:32 +0100

    Add top bar

---
+++ packages/client3/components/design-system/topbar/topbar.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { TopBar } from "./index";
+
+export default {
+  title: "FAD/Components/TopBar",
+  component: TopBar,
+  argTypes: {
+    topBarTitle: {
+      control: {
+        type: "text",
+      },
+    },
+  },
+} as ComponentMeta<typeof TopBar>;
+
+export const TopBarStory: ComponentStory<typeof TopBar> = (args) => {
+  return <TopBar {...args} />;
+};
+
+TopBarStory.args = {
+  topBarTitle: "All Artist Pools",
+  avatarUrl:
+    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAdUlEQVR42mNgGAWjAAj+48GUG37i92+cmFJL/hMDKLHkv1TeVYKYIgvwBQ81gommFvxHtqB0797/6BbCxMixAGzA7AcPUFyJzEcWI9sHxAQP1YIIGWPzCVUjeehbQLN8gK2wG1o+oElpSiiIqFoXUKuCoboFAP+MJG7jSOWlAAAAAElFTkSuQmCC",
+};
