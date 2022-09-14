3278d160ca7369cc66024da8a1da39e1e2ea4a2c
Sacha <sacha@weavik.com>
2022-09-14 11:13:11 -0400

    [FAD-53] TextArea

---
+++ packages/client3/components/design-system/input/text-area.stories.tsx
@@ -0,0 +1,17 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import { TextArea } from ".";
+
+export default {
+  component: TextArea,
+  title: "FAD/Components/Input",
+} as ComponentMeta<typeof TextArea>;
+
+export const TextAreaStory: ComponentStory<typeof TextArea> = (args) => (
+  <TextArea {...args} />
+);
+
+TextAreaStory.args = {
+  placeholder: "Text Here",
+  textSize: "md",
+};
