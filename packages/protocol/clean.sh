53f2084faf351c712555f5b86ca0985955c16103
Constantin Dogaru <ctin.dogaru95@gmail.com>
2022-09-23 15:35:59 +0300

    remove the aurora deployment

--- packages/protocol/clean.sh
+++ packages/protocol/clean.sh
@@ -5,3 +5,7 @@ rm -rf deployments/localhost/
 rm -rf deployments/all_dev.json
 rm -rf ../client/config/pool-metadata/localhost.json
 git checkout ../client2/constants/metadata/localhost.json
+
+rm -rf .openzeppelin/unknown-1313161555.json
+rm -rf deployments/aurora/
+rm -rf ../client/config/pool-metadata/aurora.json
