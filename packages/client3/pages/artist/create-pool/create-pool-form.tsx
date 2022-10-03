361fe7e3a1a08f7b35cdf84740db0d72cb183952
v1n33th <vineeth007@hotmail.co.in>
2022-10-03 10:53:16 -0400

    [FAD-83] Integrated Goldfinch factory with useContract function added dummy call in create pool screen(needs to be changed later)

--- packages/client3/pages/artist/create-pool/create-pool-form.tsx
+++ packages/client3/pages/artist/create-pool/create-pool-form.tsx
@@ -6,6 +6,7 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import { Button, Form } from "@/components/design-system";
 import { Divider } from "@/components/design-system/divider";
 import { UploadedFileType } from "@/components/upload-pdf";
+import { useContract } from "@/lib/contracts";
 import { useWallet } from "@/lib/wallet"; // add wallet address
 
 import CreatePoolDetailEntry from "./create-pool-detail-entry";
@@ -41,22 +42,51 @@ function CreatePoolForm() {
   const { account } = useWallet();
   const [isLoading, setIsLoading] = useState(false);
 
+  const BORROWER = "0x2D0113824068e9c5fc106772abC583BF8e19597A";
+  const GOLDFINCH_FACTORY = "0xc2872Dc1AC3da8e8074685b86Fb80522182Ef564";
+  const JUNIOR_FEE_PERCENT = "20";
+  const LIMIT = "10000000000";
+  const INTEREST_APR = "50000000000000000"; // 5% APR
+  const PAYMENT_PERIOD_IN_DAYS = "10";
+  const TERM_IN_DAYS = "365";
+  const LATE_FEE_APR = "0";
+  const PRINCIPAL_GRACE_PERIOD_IN_DAYS = "185";
+  const FUNDABLE_AT = "0";
+  const ALLOWED_UID = [0];
+  const goldfinchFactory = useContract(
+    "GoldfinchFactory",
+    "0xd20508E1E971b80EE172c73517905bfFfcBD87f9"
+  );
+
   const onSubmit: SubmitHandler<FormFields> = async (data) => {
     setIsLoading(true);
-    await axios.post(`/api/pool`, {
-      params: {
-        ...data,
-        walletAddress: account,
-        closingDate: new Date(data.closingDate.setHours(0, 0, 0, 0)),
-      },
-    });
+    const receipt = await goldfinchFactory?.createPool(
+      BORROWER,
+      JUNIOR_FEE_PERCENT,
+      LIMIT,
+      INTEREST_APR,
+      PAYMENT_PERIOD_IN_DAYS,
+      TERM_IN_DAYS,
+      LATE_FEE_APR,
+      PRINCIPAL_GRACE_PERIOD_IN_DAYS,
+      FUNDABLE_AT,
+      ALLOWED_UID
+    );
+    console.log("Receipt", receipt);
+    // await axios.post(`/api/pool`, {
+    //   params: {
+    //     ...data,
+    //     walletAddress: account,
+    //     closingDate: new Date(data.closingDate.setHours(0, 0, 0, 0)),
+    //   },
+    // });
     router.push("/artist/dashboard");
     setIsLoading(false);
   };
 
   return (
     <Form rhfMethods={rhfMethods} onSubmit={onSubmit}>
-      <div className="grid grid-cols-4 gap-y-6 px-64">
+      {/* <div className="grid grid-cols-4 gap-y-6 px-64">
         <CreatePoolDetailEntry
           control={control}
           register={register}
@@ -70,7 +100,7 @@ function CreatePoolForm() {
         <Divider className="col-span-4" />
 
         <CreatePoolTerms formState={formState} register={register} />
-      </div>
+      </div> */}
 
       <div className="float-right my-10 flex gap-x-2 px-64">
         <Button
