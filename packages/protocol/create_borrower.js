97ad8580ecc8d337b4410177d11a239620af67e3
Constantin Dogaru <ctin.dogaru95@gmail.com>
2022-10-06 11:57:22 +0300

    add more pools

--- packages/protocol/create_borrower.js
+++ packages/protocol/create_borrower.js
@@ -1,20 +1,18 @@
 const hre = require("hardhat")
 
-// 0x59FD62a8d4B2aE2d5ae6A519B2FE4f87f65845D9 - Constantin, 2nd account
 // 0x108Cc3833CD49333A7908e4bB52f4CF8f4090425 - Sacha
 // 0xf6E574430ddC45454c469A8f8f140a14590FF9dF - Vineeth
 // 0x062765d960DEa3e15cdeB8313ECE1b556FE23FCf - Luan
 // 0x9456f8a32a4Cd3750eA551aaC70712e0e374d052 - Harrison
 // 0x86D637d8EB368BC61E4A2111D11050B299B2de2c - Christopher
 const ARTISTS = [
-  // "0x59FD62a8d4B2aE2d5ae6A519B2FE4f87f65845D9",
   "0x108Cc3833CD49333A7908e4bB52f4CF8f4090425",
   "0xf6E574430ddC45454c469A8f8f140a14590FF9dF",
   "0x062765d960DEa3e15cdeB8313ECE1b556FE23FCf",
   "0x9456f8a32a4Cd3750eA551aaC70712e0e374d052",
   "0x86D637d8EB368BC61E4A2111D11050B299B2de2c",
 ]
-const GOLDFINCH_FACTORY = "0xc2872Dc1AC3da8e8074685b86Fb80522182Ef564"
+const GOLDFINCH_FACTORY = "0xeD785942bcC7c0E8Ae0E444d8B212F2B3e662e7c"
 
 async function main() {
   const GoldfinchFactory = await hre.ethers.getContractFactory("GoldfinchFactory")
@@ -25,7 +23,7 @@ async function main() {
     const result = await receipt.wait()
     const address = getBorrowerAddress(result)
     console.log(address)
-    await new Promise((r) => setTimeout(r, 6000))
+    // await new Promise((r) => setTimeout(r, 6000))
   }
 }
 
